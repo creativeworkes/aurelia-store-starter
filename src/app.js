1b0ac370bdb56a390455e0cc743968d0a82ce96d
yeqinglan <yeqinglan@hd123.com>
2019-12-12 11:15:31 +0800

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,28 +1,16 @@
-import { inject } from 'aurelia-framework';
-import { Store } from 'aurelia-store';
+import {connectTo} from 'aurelia-store';
 import * as Actions from './state/actions';
 
-@inject(Store)
+@connectTo()
 export class App {
 
   task;
 
   constructor(store) {
-    this.store = store;
     this.actions = Actions;
   }
 
   addTodo() {
     this.actions.addTodo(this.task);
-  }
-
-  bind() {
-    this.subscription = this.store.state.subscribe(
-      (state) => this.state = state
-    );
-  }
-
-  unbind() {
-    this.subscription.unsubscribe();
   }
 }
